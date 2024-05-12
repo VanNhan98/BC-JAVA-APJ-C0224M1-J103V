a9b7d4967349655338bdeceae93d109515742e47
Mai Van Nhan <gplgarena@gmail.com>
2024-05-12 15:39:30 +0700

    add file

---
+++ access-modifier/static-method/src/Student.java
@@ -0,0 +1,20 @@
+public class Student {
+    private int id;
+    private String name;
+    private static  String college = "BBDIT";
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    static void change() {
+        college = "CODEGYM";
+    }
+
+    void display() {
+        System.out.println(id + " " + name + " " + college);
+    }
+
+
+}
